253df27010d3bbd382c3fe3189dde424009d6cfa
rulewrite <rulewrite@gmail.com>
2022-04-08 00:06:14 +0900

    feat: Shadowing 예제 추가

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -10,4 +10,14 @@ fn main() {
   // 컴파일 중에 결과를 평가할 수 있으므로 10,800처럼 난해하지 않고, 이해하기 쉽도록 선언.
   const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
   println!("The value of x is: {}", THREE_HOURS_IN_SECONDS);
+
+  // Shadowing
+  // 같은 이름의 새 변수를 선언할 수 있다. 이전 변수를 가리게(Shadowing) 됨.
+  let y = 5;
+  let y = y + 1;
+  {
+    let y = y * 2;
+    println!("내부 스코프에서 y 변수의 값은: {}", y); // 12
+  }
+  println!("y 변수의 값은: {}", y); // 6
 }
