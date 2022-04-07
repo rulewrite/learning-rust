dea3665a8a648fe9bc79fab171ee2d7dbce520be
rulewrite <rulewrite@gmail.com>
2022-04-07 23:46:24 +0900

    feat: 변수 설정 예제

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,3 +1,7 @@
 fn main() {
-  println!("Hello, world!");
+  // 변수는 기본적으로 불변. `mut`을 선언하여 가변성 변수로 설정 가능
+  let mut x = 5;
+  println!("The value of x is : {}", x);
+  x = 6;
+  println!("The value of x is: {}", x);
 }
