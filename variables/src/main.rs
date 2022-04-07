da14a33bc31c79ba8422b42d317a08d2bcdda2bd
rulewrite <rulewrite@gmail.com>
2022-04-07 23:56:01 +0900

    feat: 상수 예제 추가

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -4,4 +4,10 @@ fn main() {
   println!("The value of x is : {}", x);
   x = 6;
   println!("The value of x is: {}", x);
+
+  // 상수는 표현식으로만 선언될 수 있다. (런타임 중에 계산되는 함수 결과 등에 사용 불가)
+  // 타입 선언을 필히 해야 함. 추론이 안되는 듯?
+  // 컴파일 중에 결과를 평가할 수 있으므로 10,800처럼 난해하지 않고, 이해하기 쉽도록 선언.
+  const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
+  println!("The value of x is: {}", THREE_HOURS_IN_SECONDS);
 }
