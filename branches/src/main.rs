f1884c905b9fa35203b02946dfdafee33dea29a5
rulewrite <rulewrite@gmail.com>
2022-04-11 12:33:06 +0900

    feat: let 구문에 if 사용하기

--- branches/src/main.rs
+++ branches/src/main.rs
@@ -24,4 +24,14 @@ fn main() {
     } else {
         println!("number is not divisible by 4, 3, or 2");
     }
+
+    let condition = true;
+    // if도 표현식이기에 let 구문 우축에 사용이 가능함.
+    let number = if condition { 5 } else { 6 }; // 코드 블럭은 마지막 표현식으로 평가되고 숫자 자체도 표현식임을 기억하자.
+    println!("The value of number is: {}", number); // 5
+
+    // 아울러 if에 속한 각 arms의 결과는 반드시 같은 타입이어야 한다. 즉 아래는 실패한다.
+    // let number = if condition { 5 } else { "six" };
+    // 변수는 단일 타입을 가져야 하고 러스트는 컴파일 시에 `number` 변수의 타입이 뭔지 정확히 알아야 하기 때문.
+    // 그래야 `number`가 사용되는 모든 곳에서 유효한지 검증이 가능하다.
 }
