156683fa27f9cf779d3557f610e9d301b47a8170
rulewrite <rulewrite@gmail.com>
2022-04-11 12:41:46 +0900

    feat: loop 레이블 예제

--- loops/src/main.rs
+++ loops/src/main.rs
@@ -6,4 +6,27 @@ fn main() {
         println!("again!");
         break;
     }
+
+    let mut count = 0;
+    // 루프에 레이블 지정
+    'counting_up: loop {
+        println!("count = {}", count);
+        let mut remaining = 10;
+
+        loop {
+            println!("remaining = {}", remaining);
+            if remaining == 9 {
+                break;
+            }
+
+            if count == 2 {
+                break 'counting_up; // 'counting_up 루프를 멈춤
+            }
+
+            remaining -= 1;
+        }
+
+        count += 1;
+    }
+    println!("End count = {}", count);
 }
