de8d21822c43d1e65f80bf8b9f01473d7d044a36
rulewrite <rulewrite@gmail.com>
2022-04-11 12:22:01 +0900

    feat: if문 예제

--- branches/src/main.rs
+++ branches/src/main.rs
@@ -1,3 +1,10 @@
 fn main() {
-    println!("Hello, world!");
+    let number = 3;
+
+    // 조건과 관련된 코드 블럭은 match arms와 같이 arms라 부름
+    if number < 5 {
+        println!("condition was true");
+    } else {
+        println!("condition was false");
+    }
 }
