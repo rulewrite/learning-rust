86c6f8d03226244aa8209a809f0009fe520eedcf
rulewrite <rulewrite@gmail.com>
2022-04-11 12:37:46 +0900

    feat: loop 예제

--- loops/src/main.rs
+++ loops/src/main.rs
@@ -1,3 +1,9 @@
 fn main() {
-    println!("Hello, world!");
+    // 러스트가 제공하는 3가지 반복문: loop, while, for
+
+    // 명시적으로 중지할 때까지 반복을 수행한다.
+    loop {
+        println!("again!");
+        break;
+    }
 }
