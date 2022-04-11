2ce1cd859360438062c535f278dacb86da55c83e
rulewrite <rulewrite@gmail.com>
2022-04-11 12:45:18 +0900

    feat: loop 반환 값 예제

--- loops/src/main.rs
+++ loops/src/main.rs
@@ -29,4 +29,16 @@ fn main() {
         count += 1;
     }
     println!("End count = {}", count);
+
+    // 스레드가 작업을 완료했는지 확인하거나, 실패할 수 있는 작업을 재시도 할 때 사용한다.
+    // 또한 해당 작업의 결과를 루프 외부로 전달할 수 있다.
+    let mut counter = 0;
+    let result = loop {
+        counter += 1;
+
+        if counter == 10 {
+            break counter * 2; // 반환 값 명시
+        }
+    };
+    println!("The result is {}", result); // 20
 }
