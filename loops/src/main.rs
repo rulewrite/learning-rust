fcb5868f24d845e2b913991d9a86669c3828f290
rulewrite <rulewrite@gmail.com>
2022-04-11 12:58:14 +0900

    feat: for문 예제

--- loops/src/main.rs
+++ loops/src/main.rs
@@ -48,4 +48,28 @@ fn main() {
         number -= 1;
     }
     println!("LIFTOFF!!!");
+
+    // `while`로 컬렉션을 순회할 수 있다.
+    let a = [10, 20, 30, 40, 50];
+    let mut index = 0;
+    while index < 5 {
+        println!("The value is: {}", a[index]);
+        index += 1;
+    }
+    // 하지만 위와 같은 경우,
+    // 에러가 발생하기 쉬우며 (배열의 인덱스를 정확히 알아야 함.)
+    // 느리다. (컴파일러가 반복될 때마다 index가 배열 범위에 포함되는지 조건 검사를 런타임 코드에 추가하기 때문)
+
+    // 따라서 for 문으로 순회하는게 좋다.
+    let a = [10, 20, 30, 40, 50];
+    for element in a {
+        println!("The value is: {}", element);
+    }
+
+    // 특정 횟수만큼 실행하는 상황에서도 표준 라이브러리에 제공되는 Range를 활용한 for문이 깔끔하다.
+    // Range는 한 숫자에서 다른 숫자의 "전"까지 모든 숫자를 차례로 생성한다.
+    for number in (1..4).rev() {
+        println!("{}!", number); // 3, 2, 1
+    }
+    println!("LIFTOFF!!!");
 }
