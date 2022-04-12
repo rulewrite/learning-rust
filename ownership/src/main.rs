f1982480fe8f659b1058e3a6567b2890e9fb2371
rulewrite <rulewrite@gmail.com>
2022-04-13 00:30:22 +0900

    feat: 함수 반환값에 소유권 이전 예제

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -17,6 +17,8 @@ fn main() {
     copy_value_in_stack();
 
     function_and_ownership();
+
+    return_and_scope();
 }
 
 fn string_scope() {
@@ -100,3 +102,27 @@ fn makes_copy(some_integer: i32) {
     // some_integer가 스코프내로 들어옴
     println!("{}", some_integer);
 } // some_integer가 스코프 밖으로 벗어났지만 별다른 일은 발생하지 않는다.
+
+/**
+ * 값의 반환도 마찬가지로 소유권을 이동시킨다.
+ */
+fn return_and_scope() {
+    let s1 = gives_ownership(); // gives_ownership은 반환 값을 s1으로 이동시킨다.
+
+    let s2 = String::from("hello"); // s2가 스코프 안에 들어옴.
+
+    let s3 = takes_and_gives_back(s2); // s2는 takes_and_gives_back 안으로 이동되며
+                                       // 반환값은 s3로 이동됨
+
+    println!("{},{}", s1, s3);
+} // s1, s3는 스코프 밖을 벗어나 drop이 호출되지만 s2는 이동됐으므로 아무런 일이 일어나지 않는다.
+
+fn gives_ownership() -> String {
+    let some_string = String::from("yours"); // some_string이 스코프 안에 들어옴
+    some_string // some_string은 호출한 함수로 이동한다.
+}
+
+fn takes_and_gives_back(a_string: String) -> String {
+    // a_string이 스코프 안으로 들어옴
+    a_string // a_string은 반환되고, 호출한 쪽의 함수로 이동됩니다.
+}
