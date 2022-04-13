c5261f51a59c84c1152d40aca3135fdf5a7e9e2c
rulewrite <rulewrite@gmail.com>
2022-04-13 22:53:30 +0900

    fix: 댕글링 참조 에러

--- references_and_borrowing/src/main.rs
+++ references_and_borrowing/src/main.rs
@@ -74,12 +74,15 @@ fn mutable_references() {
 
 fn dangling_references() {
     let reference_to_nothing = dangle();
+
+    println!("{}", reference_to_nothing);
 }
 
 // 아래 함수는 컴파일 에러가 발생한다. "이 함수의 반환 타입은 빌린 값을 포함하고 있는데, 빌려온 실제 값은 없습니다."
-// dangle은 String의 참조자를 반환
-fn dangle() -> &String {
+// dangle은 String의 참조자를 반환 -> String을 직접 반환
+fn dangle() -> String {
     let s = String::from("hello"); // s는 새로운 String
 
-    &s // String s의 참조자를 반환
+    // &s // String s의 참조자를 반환
+    s // String을 반환하여 소유권 이전
 } // 여기서 s는 스코프를 벗어나 버려지므로 메모리는 사라진다.
