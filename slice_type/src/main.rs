32805660cd5719ccbffd5b0ad5c09ff401e982e0
rulewrite <rulewrite@gmail.com>
2022-04-14 00:11:41 +0900

    feat: 첫번째 단어 잘라내기 예제

--- slice_type/src/main.rs
+++ slice_type/src/main.rs
@@ -19,4 +19,29 @@ fn main() {
     println!("{}", hello);
     println!("{}", world);
     println!("{}", hello_world);
+
+    // let mut s2 = String::from("hello world");
+    let s2 = String::from("hello world");
+    let word = first_word(&s2);
+    // s2.clear(); // 앞서 불변 참조자를 만들었으니 가변 참조자는 불가하다. clear 내에서 가변 참조자를 갖기 위한 시도를 하기 때문에 해당 코드는 컴파일이 실패한다.
+    println!("{}", word);
+}
+
+// 반환값은 슬라이스 시작 위치 참조와 요소 수로 이루어짐 (슬라이스 타입)
+fn first_word(s: &String) -> &str {
+    let bytes = s.as_bytes(); // 바이트 배열로 변환
+
+    for (i, &item) in bytes // 튜플 구조 해제
+        // 바이트 배열의 반복자를 생성
+        .iter()
+        // 반복 결과를 (index, 요소에 대한 참조값) 튜플로 만들어 반환
+        .enumerate()
+    {
+        // 바이트 리터럴 문법으로 공백 문자를 나타내는 바이트 값을 알아냄
+        if item == b' ' {
+            return &s[0..i];
+        }
+    }
+
+    &s[..]
 }
