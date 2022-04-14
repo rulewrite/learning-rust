bcff41a4586bba331c8e9588f93e39985a7f5b04
rulewrite <rulewrite@gmail.com>
2022-04-14 12:19:57 +0900

    feat: User 구조체 정의 및 인스턴스 생성

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -1,3 +1,18 @@
+// 튜플 처럼 구조체의 구성요소들은 각자 다른 타입을 가질 수 있음
+// 각 구성요소들을 명명할 수 있어 값의 의미를 명확하게 할 수 있고 순서에 의존없이 접근할 수 있어 보다 유연함.
+struct User {
+    username: String, // field
+    email: String,
+    sign_in_count: u64,
+    active: bool,
+}
+
 fn main() {
-    println!("Hello, world!");
+    // 구조체는 정의된 양식이며 User 인스턴스 생성(실체화)
+    let user1 = User {
+        email: String::from("someone@example.com"),
+        username: String::from("rulewrite"),
+        active: true,
+        sign_in_count: 1,
+    };
 }
