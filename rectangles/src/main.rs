8aa0baf89a73f55c4d2b6b8fb17e91ff025eff36
rulewrite <rulewrite@gmail.com>
2022-04-20 12:17:58 +0900

    feat: Rectangle 디버깅 출력 추가

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -1,3 +1,4 @@
+#[derive(Debug)]
 struct Rectangle {
     width: u32,
     height: u32,
@@ -12,7 +13,9 @@ fn main() {
     println!(
         "The area of the rectangle is {} square pixels.",
         area(&rect1)
-    )
+    );
+
+    println!("rect1 is {:?}", rect1);
 }
 
 fn area(rectangle: &Rectangle) -> u32 {
