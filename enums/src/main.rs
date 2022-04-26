995a0241f7e31f57f4ac316a296072aa65f48e19
rulewrite <rulewrite@gmail.com>
2022-04-26 20:39:15 +0900

    feat: 열거형의 메소드 정의

--- enums/src/main.rs
+++ enums/src/main.rs
@@ -18,6 +18,13 @@ enum Message {
     ChangeColor(i32, i32, i32), // 세개의 i32
 }
 
+// 열거형도 메소드를 정의할 수 있다.
+impl Message {
+    fn call(&self) {
+        // 메소드 내용 정의
+    }
+}
+
 // 위 Message 열거형은 다음과 같이 분리될 수 있다. 하지만 모든 variants가 `Message` 타입으로 그룹화되며
 // 아래 처럼 각기 다른 타입을 갖는 여러 구조체를 사용한다면 메시지 중 어떤 한가지를 인자로 받는 함수를 정의하기 어렵다.
 struct QuitMessage; // 유닛 구조체
@@ -40,6 +47,9 @@ fn main() {
     // 열거형을 정의하면 이 생성자 함수가 자동으로 정의된다.
     let home = IpAddr::V4(127, 0, 0, 1);
     let loopback = IpAddr::V6(String::from("::1"));
+
+    let m = Message::Write(String::from("Hello"));
+    m.call();
 }
 
 // 열거형 타입을 받는 함수를 정의할 수 있다.
