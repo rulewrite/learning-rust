f9b20552c1a0dc6e120d28579090d4c28ac44c5e
rulewrite <rulewrite@gmail.com>
2022-05-01 12:39:25 +0900

    feat: match 예제

--- match_example/src/main.rs
+++ match_example/src/main.rs
@@ -1,3 +1,22 @@
+enum Coin {
+    Penny, // variants
+    Nickel,
+    Dime,
+    Quarter,
+}
+
+fn value_on_cents(coin: Coin) -> u32 {
+    match coin {
+        Coin::Penny => {
+            println!("Lucky penny!");
+            1
+        }
+        Coin::Nickel => 5, // arms
+        Coin::Dime => 10,
+        Coin::Quarter => 25,
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("{}", value_on_cents(Coin::Penny));
 }
