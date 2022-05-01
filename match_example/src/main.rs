4ca8ddfb7f4b20aa131b2e6087a808830831d0c5
rulewrite <rulewrite@gmail.com>
2022-05-01 13:46:43 +0900

    feat: 값을 갖는 variants 값에 액세스

--- match_example/src/main.rs
+++ match_example/src/main.rs
@@ -1,8 +1,15 @@
+#[derive(Debug)] // So we can inspect the state in a minute
+enum UsState {
+    Alabama,
+    Alaska,
+    // --snip--
+}
+
 enum Coin {
     Penny, // variants
     Nickel,
     Dime,
-    Quarter,
+    Quarter(UsState), // 내부에 UsState 값을 갖도록 추가
 }
 
 fn value_on_cents(coin: Coin) -> u32 {
@@ -13,10 +20,13 @@ fn value_on_cents(coin: Coin) -> u32 {
         }
         Coin::Nickel => 5, // arms
         Coin::Dime => 10,
-        Coin::Quarter => 25,
+        Coin::Quarter(state) => {
+            println!("State quarter from {:?}!", state);
+            25
+        }
     }
 }
 
 fn main() {
-    println!("{}", value_on_cents(Coin::Penny));
+    println!("{}", value_on_cents(Coin::Quarter(UsState::Alaska)));
 }
