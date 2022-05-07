671a74ef5e565e6f6e4d1622996d2ae8c017f188
rulewrite <rulewrite@gmail.com>
2022-05-07 21:46:42 +0900

    feat: network, client 모듈 추가 및 내부 함수 정의

--- communicator/src/lib.rs
+++ communicator/src/lib.rs
@@ -1,3 +1,15 @@
+mod client {
+    fn connect() {}
+}
+
+// mod 모듈명
+mod network {
+    // 이 내부의 요소들은 네임스페이스 network 안에 존재
+
+    // network::connect()로 호출
+    fn connect() {}
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
