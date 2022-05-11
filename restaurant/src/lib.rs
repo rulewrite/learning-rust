1c510d36bff9b98afa62159b03b88c63841248cc
rulewrite <rulewrite@gmail.com>
2022-05-11 23:25:47 +0900

    feat: 모듈 내에 요소 접근하기

--- restaurant/src/lib.rs
+++ restaurant/src/lib.rs
@@ -2,8 +2,8 @@
 mod front_of_house {
     // 모듈 내부에는 다른 모듈이 존재할 수 있다.
     // 아울러 구조체, 열거형, 상수, 트레잇, 함수 등을 보유 할 수 있다.
-    mod hosting {
-        fn add_to_waitlist() {}
+    pub mod hosting {
+        pub fn add_to_waitlist() {}
 
         fn seat_at_table() {}
     }
@@ -15,6 +15,14 @@ mod front_of_house {
 
         fn take_payment() {}
     }
+}
+
+pub fn eat_at_restaurant() {
+    // Absolute path
+    crate::front_of_house::hosting::add_to_waitlist();
+
+    // Relative path
+    front_of_house::hosting::add_to_waitlist();
 }
 
 #[cfg(test)]
