5a18222063f0f69952ff1d8e344c340c6ff79e96
rulewrite <rulewrite@gmail.com>
2022-05-11 21:44:09 +0900

    feat: super로 부모 모듈에 접근하기

--- communicator/src/lib.rs
+++ communicator/src/lib.rs
@@ -8,11 +8,17 @@ pub mod client; // contents of client.rs
 // mod 모듈명
 pub mod network;
 
+// 특별히 어노테이션이 붙어있을 뿐 tests 또한 모듈이다. 따라서 모듈 계층 구조는 다음과 같다.
+// communicator
+//  ├── client
+//  ├── network
+//  |   └── server
+//  └── tests
 #[cfg(test)]
 mod tests {
     #[test]
     fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+        // 테스트 모듈 내이기 때문에 부모로 할번 올라갔다가 client로 접근할 필요가 있다.
+        super::client::connect();
     }
 }
