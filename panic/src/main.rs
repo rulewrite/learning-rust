4dc2400ba485c21c3de1adc1440623f634b9c77f
rulewrite <rulewrite@gmail.com>
2022-06-11 00:50:16 +0900

    feat: 에러 원인에 따른 처리 분기

--- panic/src/main.rs
+++ panic/src/main.rs
@@ -1,4 +1,5 @@
 use std::fs::File;
+use std::io::ErrorKind;
 
 fn main() {
     // panic!("crash and burn");
@@ -10,6 +11,14 @@ fn main() {
 
     let f = match f {
         Ok(file) => file,
-        Err(error) => panic!("Problem opening the file: {:?}", error),
+        Err(error) => match error.kind() {
+            ErrorKind::NotFound => match File::create("hello.txt") {
+                Ok(file) => file,
+                Err(e) => panic!("Problem creating the file: {:?}", e),
+            },
+            other_error => {
+                panic!("Problem opening the file: {:?}", other_error)
+            }
+        },
     };
 }
