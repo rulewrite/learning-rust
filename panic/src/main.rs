fb18f1b37783bd091e8a5273ff08c734ad012b7e
rulewrite <rulewrite@gmail.com>
2022-06-11 00:24:53 +0900

    feat: 실패할 수 있는 작업 예외 처리

--- panic/src/main.rs
+++ panic/src/main.rs
@@ -1,6 +1,15 @@
+use std::fs::File;
+
 fn main() {
     // panic!("crash and burn");
 
-    let v = vec![1, 2, 3];
-    v[99];
+    // let v = vec![1, 2, 3];
+    // v[99];
+
+    let f = File::open("hello.txt");
+
+    let f = match f {
+        Ok(file) => file,
+        Err(error) => panic!("Problem opening the file: {:?}", error),
+    };
 }
