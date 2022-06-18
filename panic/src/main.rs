8d580cf09965c882031b8d193fe5491b19eda358
rulewrite <rulewrite@gmail.com>
2022-06-18 17:43:56 +0900

    feat: 에러 전파

--- panic/src/main.rs
+++ panic/src/main.rs
@@ -1,5 +1,5 @@
 use std::fs::File;
-use std::io::ErrorKind;
+use std::io::{self, ErrorKind, Read};
 
 fn main() {
     // panic!("crash and burn");
@@ -22,7 +22,25 @@ fn main() {
         },
     };
 
-    let f = File::open("hello2.txt").unwrap();
+    // let f = File::open("hello2.txt").unwrap();
 
-    let f = File::open("hello3.txt").expect("Failed to open hello.txt");
+    // let f = File::open("hello3.txt").expect("Failed to open hello.txt");
+
+    read_username_from_file();
+}
+
+fn read_username_from_file() -> Result<String, io::Error> {
+    let f = File::open("hello4.txt");
+
+    let mut f = match f {
+        Ok(file) => file,
+        Err(e) => return Err(e),
+    };
+
+    let mut s = String::new();
+
+    match f.read_to_string(&mut s) {
+        Ok(_) => Ok(s),
+        Err(e) => Err(e),
+    }
 }
