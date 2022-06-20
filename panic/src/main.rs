af61b41726d8e9ce63111a52fe3726cb1724e5b7
rulewrite <rulewrite@gmail.com>
2022-06-20 15:51:17 +0900

    feat: 커스텀 타입 추가

--- panic/src/main.rs
+++ panic/src/main.rs
@@ -16,3 +16,21 @@ fn read_username_from_file2() -> Result<String, io::Error> {
 fn last_char_of_first_line(text: &str) -> Option<char> {
     text.lines().next()?.chars().last()
 }
+
+pub struct Guess {
+    value: i32,
+}
+
+impl Guess {
+    pub fn new(value: i32) -> Guess {
+        if value < 1 || value > 100 {
+            panic!("Guess value must be between 1 and 100, got {}.", value);
+        }
+
+        Guess { value }
+    }
+
+    pub fn value(&self) -> i32 {
+        self.value
+    }
+}
