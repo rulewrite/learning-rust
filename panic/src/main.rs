05ce36b065da933259a97152a565067c94bf4d44
rulewrite <rulewrite@gmail.com>
2022-06-20 13:08:15 +0900

    feat: Option에 ? 연산자 사용하기

--- panic/src/main.rs
+++ panic/src/main.rs
@@ -2,48 +2,7 @@ use std::fs::File;
 use std::io::{self, ErrorKind, Read};
 
 fn main() {
-    // panic!("crash and burn");
-
-    // let v = vec![1, 2, 3];
-    // v[99];
-
-    let f = File::open("hello.txt");
-
-    let f = match f {
-        Ok(file) => file,
-        Err(error) => match error.kind() {
-            ErrorKind::NotFound => match File::create("hello.txt") {
-                Ok(file) => file,
-                Err(e) => panic!("Problem creating the file: {:?}", e),
-            },
-            other_error => {
-                panic!("Problem opening the file: {:?}", other_error)
-            }
-        },
-    };
-
-    // let f = File::open("hello2.txt").unwrap();
-
-    // let f = File::open("hello3.txt").expect("Failed to open hello.txt");
-
-    read_username_from_file();
-    read_username_from_file2();
-}
-
-fn read_username_from_file() -> Result<String, io::Error> {
-    let f = File::open("hello4.txt");
-
-    let mut f = match f {
-        Ok(file) => file,
-        Err(e) => return Err(e),
-    };
-
-    let mut s = String::new();
-
-    match f.read_to_string(&mut s) {
-        Ok(_) => Ok(s),
-        Err(e) => Err(e),
-    }
+    let f = File::open("hello.txt")?;
 }
 
 fn read_username_from_file2() -> Result<String, io::Error> {
@@ -52,4 +11,8 @@ fn read_username_from_file2() -> Result<String, io::Error> {
     File::open("hello.txt")?.read_to_string(&mut s)?;
 
     Ok(s)
+}
+
+fn last_char_of_first_line(text: &str) -> Option<char> {
+    text.lines().next()?.chars().last()
 }
