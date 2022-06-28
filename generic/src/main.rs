5019a3d91675f35c45fdbef38d71ce7db378373d
rulewrite <rulewrite@gmail.com>
2022-06-29 02:12:26 +0900

    feat: 구조체와 다른 제네릭 타입을 받는 메소드 정의

--- generic/src/main.rs
+++ generic/src/main.rs
@@ -27,6 +27,20 @@ impl Point<f32> {
     }
 }
 
+struct Point2<X1, Y1> {
+    x: X1,
+    y: Y1,
+}
+
+impl<X1, Y1> Point2<X1, Y1> {
+    fn mixup<X2, Y2>(self, other: Point2<X2, Y2>) -> Point2<X1, Y2> {
+        Point2 {
+            x: self.x,
+            y: other.y,
+        }
+    }
+}
+
 fn main() {
     let number_list = vec![34, 50, 25, 100, 65];
 
@@ -40,4 +54,11 @@ fn main() {
 
     let p = Point { x: 5, y: 10 };
     println!("p.x = {}", p.x());
+
+    let p1 = Point2 { x: 5, y: 10.4 };
+    let p2 = Point2 { x: "Hello", y: 'c' };
+
+    let p3 = p1.mixup(p2);
+
+    println!("p3.x = {}, p3.y = {}", p3.x, p3.y);
 }
