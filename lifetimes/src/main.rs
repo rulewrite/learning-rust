2c245bfbafb6325486f79cb351dbf9f5fb68ed8f
rulewrite <rulewrite@gmail.com>
2022-07-01 03:17:55 +0900

    feat: 서로 다른 라이프타임을 가진 문자열 참조를 이용한 longest 함수 호출

--- lifetimes/src/main.rs
+++ lifetimes/src/main.rs
@@ -12,4 +12,12 @@ fn main() {
 
     let result = longest(string1.as_str(), string2);
     println!("The longest string is {}", result);
+
+    let string1 = String::from("long string is long");
+
+    {
+        let string2 = String::from("xyz");
+        let result = longest(string1.as_str(), string2.as_str());
+        println!("The longest string is {}", result);
+    }
 }
