117f0bee33efd962cfea2d8bd7dec5ddb4a9c45b
rulewrite <rulewrite@gmail.com>
2022-06-30 23:02:26 +0900

    feat: clone을 사용하여 참조를 반환도록 변경

--- generic/src/main.rs
+++ generic/src/main.rs
@@ -1,7 +1,7 @@
-fn largest<T: PartialOrd + Copy>(list: &[T]) -> T {
-    let mut largest = list[0];
+fn largest<T: PartialOrd + Clone>(list: &[T]) -> &T {
+    let mut largest = &list[0];
 
-    for &item in list {
+    for item in list {
         if item > largest {
             largest = item;
         }
