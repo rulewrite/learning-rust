133a13e5d89ed13896001e375032ef977d4cb418
rulewrite <rulewrite@gmail.com>
2022-07-01 01:13:07 +0900

    feat: 트레잇 바운드로 조건부 메소드 구현

--- trait_example/src/lib.rs
+++ trait_example/src/lib.rs
@@ -1,3 +1,5 @@
+use std::fmt::Display;
+
 pub trait Summary {
   fn summarize_author(&self) -> String;
 
@@ -25,5 +27,26 @@ pub struct Tweet {
 impl Summary for Tweet {
   fn summarize_author(&self) -> String {
     format!("@{}", self.username)
+  }
+}
+
+struct Pair<T> {
+  x: T,
+  y: T,
+}
+
+impl<T> Pair<T> {
+  fn new(x: T, y: T) -> Self {
+    Self { x, y }
+  }
+}
+
+impl<T: Display + PartialOrd> Pair<T> {
+  fn cmp_display(&self) {
+    if self.x >= self.y {
+      println!("The largest member is x = {}", self.x);
+    } else {
+      println!("The largest member is y = {}", self.y);
+    }
   }
 }
