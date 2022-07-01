8337760ed7f6d96d5c533abafea6f5fb633c582b
rulewrite <rulewrite@gmail.com>
2022-07-01 14:13:56 +0900

    feat: 제네릭 타입 파라미터, 트레잇 바운드, 라이프타임을 갖는 함수 추가

--- lifetimes/src/main.rs
+++ lifetimes/src/main.rs
@@ -1,3 +1,5 @@
+use std::fmt::Display;
+
 fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
     if x.len() > y.len() {
         x
@@ -20,6 +22,18 @@ impl<'a> ImportantExcerpt<'a> {
     fn announce_and_return_part(&self, announcement: &str) -> &str {
         println!("Attention please: {}", announcement);
         self.part
+    }
+}
+
+fn longest_with_an_announcement<'a, T>(x: &'a str, y: &'a str, ann: T) -> &'a str
+where
+    T: Display,
+{
+    println!("Announcement! {}", ann);
+    if x.len() > y.len() {
+        x
+    } else {
+        y
     }
 }
 
