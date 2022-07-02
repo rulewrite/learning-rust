14a88d8c671711c7dd3e7f7a7b6a8203854ecf7d
rulewrite <rulewrite@gmail.com>
2022-07-02 20:42:06 +0900

    feat: assert_eq! 매크로로 동치 테스트

--- adder/src/lib.rs
+++ adder/src/lib.rs
@@ -10,6 +10,10 @@ impl Rectangle {
     }
 }
 
+pub fn add_two(a: i32) -> i32 {
+    a + 2
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -42,5 +46,10 @@ mod tests {
         };
 
         assert!(!smaller.can_hold(&larger));
+    }
+
+    #[test]
+    fn it_adds_two() {
+        assert_eq!(4, add_two(2));
     }
 }
