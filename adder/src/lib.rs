94352269eb43cbc7b921348778a0aa71040b4cc4
rulewrite <rulewrite@gmail.com>
2022-07-02 21:54:51 +0900

    feat: 테스트 실패 메시지에 커스텀 메시지 추가

--- adder/src/lib.rs
+++ adder/src/lib.rs
@@ -14,6 +14,10 @@ pub fn add_two(a: i32) -> i32 {
     a + 2
 }
 
+pub fn greeting(name: &str) -> String {
+    format!("Hello {}!", name)
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -51,5 +55,15 @@ mod tests {
     #[test]
     fn it_adds_two() {
         assert_eq!(4, add_two(2));
+    }
+
+    #[test]
+    fn greeting_contains_name() {
+        let result = greeting("Carol");
+        assert!(
+            result.contains("Carol"),
+            "Greeting did not contain name, value was `{}`",
+            result
+        );
     }
 }
