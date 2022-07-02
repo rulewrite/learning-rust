653e2bb876aed2ce804f7c45914849d4a0f4227f
rulewrite <rulewrite@gmail.com>
2022-07-02 22:21:42 +0900

    feat: 예상되는 패닉 메시지를 추가하여 의도한 패닉이 발생하는지 확인

--- adder/src/lib.rs
+++ adder/src/lib.rs
@@ -24,8 +24,16 @@ pub struct Guess {
 
 impl Guess {
     pub fn new(value: i32) -> Guess {
-        if value < 1 || value > 100 {
-            panic!("Guess value must be between 1 and 100, got {}.", value);
+        if value < 1 {
+            panic!(
+                "Guess value must be greater than or equal to 1, got {}.",
+                value
+            );
+        } else if value > 100 {
+            panic!(
+                "Guess value must be less than or equal to 100, got {}.",
+                value
+            );
         }
 
         Guess { value }
@@ -82,7 +90,7 @@ mod tests {
     }
 
     #[test]
-    #[should_panic]
+    #[should_panic(expected = "Guess value must be less than or equal to 100")]
     fn greater_than_100() {
         Guess::new(200);
     }
