40f522bf6b0518f653bf8b8789838cae98124032
rulewrite <rulewrite@gmail.com>
2022-07-02 21:59:28 +0900

    feat: should_panic 속성으로 패닉이 일어나야 하는 경우 테스트

--- adder/src/lib.rs
+++ adder/src/lib.rs
@@ -18,6 +18,20 @@ pub fn greeting(name: &str) -> String {
     format!("Hello {}!", name)
 }
 
+pub struct Guess {
+    value: i32,
+}
+
+impl Guess {
+    pub fn new(value: i32) -> Guess {
+        if value < 1 || value > 100 {
+            panic!("Guess value must be between 1 and 100, got {}.", value);
+        }
+
+        Guess { value }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -65,5 +79,11 @@ mod tests {
             "Greeting did not contain name, value was `{}`",
             result
         );
+    }
+
+    #[test]
+    #[should_panic]
+    fn greater_than_100() {
+        Guess::new(200);
     }
 }
