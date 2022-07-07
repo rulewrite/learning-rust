895753b698435342e503a96981c54524183921a9
rulewrite <rulewrite@gmail.com>
2022-07-07 19:01:32 +0900

    feat: 구조체 정의에 라이프타임 명시

--- lifetimes/src/main.rs
+++ lifetimes/src/main.rs
@@ -6,6 +6,10 @@ fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
     }
 }
 
+struct ImportantExcerpt<'a> {
+    part: &'a str,
+}
+
 fn main() {
     let string1 = String::from("abcd");
     let string2 = "xyz";
@@ -20,4 +24,10 @@ fn main() {
         let result = longest(string1.as_str(), string2.as_str());
         println!("The longest string is {}", result);
     }
+
+    let novel = String::from("Call me Ishmael. Some years ago...");
+    let first_sentence = novel.split('.').next().expect("Could not find a '.'");
+    let i = ImportantExcerpt {
+        part: first_sentence,
+    };
 }
