e922cb4518207ea40ed14485928369e0d25509b6
rulewrite <rulewrite@gmail.com>
2022-07-10 16:57:55 +0900

    feat: FnMut를 구현하는 클로저

--- clousure/src/main.rs
+++ clousure/src/main.rs
@@ -17,4 +17,32 @@ fn main() {
 
     borrows_mutably();
     println!("After calling closure: {:?}", list);
+
+    sort_example();
+}
+
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+fn sort_example() {
+    let mut list = [
+        Rectangle {
+            width: 10,
+            height: 1,
+        },
+        Rectangle {
+            width: 3,
+            height: 5,
+        },
+        Rectangle {
+            width: 7,
+            height: 12,
+        },
+    ];
+
+    list.sort_by_key(|r| r.width);
+    println!("{:#?}", list);
 }
