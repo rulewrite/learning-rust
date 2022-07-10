5a252aa80657e9080b571aa5793affc2eb71a901
rulewrite <rulewrite@gmail.com>
2022-07-10 14:42:04 +0900

    feat: 가변 빌림 클로저

--- clousure/src/main.rs
+++ clousure/src/main.rs
@@ -1,4 +1,5 @@
 fn main() {
+    // 불변 빌림
     let list = vec![1, 2, 3];
     println!("Before defining closure: {:?}", list);
 
@@ -6,5 +7,14 @@ fn main() {
 
     println!("Before calling closure: {:?}", list);
     only_borrows();
+    println!("After calling closure: {:?}", list);
+
+    // 가변 빌림
+    let mut list = vec![1, 2, 3];
+    println!("Before defining closure: {:?}", list);
+
+    let mut borrows_mutably = || list.push(7);
+
+    borrows_mutably();
     println!("After calling closure: {:?}", list);
 }
