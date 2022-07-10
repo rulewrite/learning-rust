43bbda0aab72a415ac6035e7fb45aed624ddcda1
rulewrite <rulewrite@gmail.com>
2022-07-10 20:56:10 +0900

    feat: 카운팅을 위해 환경 값을 가변 참조하는 클로저

--- clousure/src/main.rs
+++ clousure/src/main.rs
@@ -19,6 +19,7 @@ fn main() {
     println!("After calling closure: {:?}", list);
 
     sort_example();
+    sort_example_fn_mut();
 }
 
 #[derive(Debug)]
@@ -45,4 +46,28 @@ fn sort_example() {
 
     list.sort_by_key(|r| r.width);
     println!("{:#?}", list);
+}
+
+fn sort_example_fn_mut() {
+    let mut list = [
+        Rectangle {
+            width: 10,
+            height: 1,
+        },
+        Rectangle {
+            width: 3,
+            height: 5,
+        },
+        Rectangle {
+            width: 7,
+            height: 12,
+        },
+    ];
+
+    let mut num_sort_operations = 0;
+    list.sort_by_key(|r| {
+        num_sort_operations += 1;
+        r.width
+    });
+    println!("{:#?}, sorted in {num_sort_operations} operations", list);
 }
