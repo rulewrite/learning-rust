efb8231658b6de08fb9dee4c1a20691d0fe0115f
rulewrite <rulewrite@gmail.com>
2022-07-10 23:30:01 +0900

    feat: 이터레이터를 소비하는 메소드 sum 사용

--- iterator/src/lib.rs
+++ iterator/src/lib.rs
@@ -9,3 +9,14 @@ fn iterator_demonstration() {
   assert_eq!(v1_iter.next(), Some(&3));
   assert_eq!(v1_iter.next(), None);
 }
+
+#[test]
+fn iterator_sum() {
+  let v1 = vec![1, 2, 3];
+
+  let v1_iter = v1.iter();
+
+  let total: i32 = v1_iter.sum();
+
+  assert_eq!(total, 6);
+}
