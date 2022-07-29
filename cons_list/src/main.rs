59a042f2d6eac258d860afaee9b4a0d4842ca12f
rulewrite <rulewrite@gmail.com>
2022-07-30 00:34:05 +0900

    feat: 참조 카운팅 cons list 정의 및 생성

--- cons_list/src/main.rs
+++ cons_list/src/main.rs
@@ -3,8 +3,19 @@ enum List {
     Nil,
 }
 
+enum RcList {
+    Cons(i32, Rc<RcList>),
+    Nil,
+}
+
 use crate::List::{Cons, Nil};
+use crate::RcList::{Cons as RcCons, Nil as RcNil};
+use std::rc::Rc;
 
 fn main() {
     let list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
+
+    let a = Rc::new(RcCons(5, Rc::new(RcCons(10, Rc::new(RcNil)))));
+    let b = RcCons(3, Rc::clone(&a));
+    let c = RcCons(4, Rc::clone(&a));
 }
