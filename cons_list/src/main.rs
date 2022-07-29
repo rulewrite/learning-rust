7a4e71bb99f04993e35fdc3f71bfd9f5baf1eec4
rulewrite <rulewrite@gmail.com>
2022-07-30 01:47:42 +0900

    feat: Rc<T> 할당 및 해제마다 참조 카운트 출력

--- cons_list/src/main.rs
+++ cons_list/src/main.rs
@@ -16,6 +16,12 @@ fn main() {
     let list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
 
     let a = Rc::new(RcCons(5, Rc::new(RcCons(10, Rc::new(RcNil)))));
+    println!("count after creating a = {}", Rc::strong_count(&a));
     let b = RcCons(3, Rc::clone(&a));
-    let c = RcCons(4, Rc::clone(&a));
+    println!("count after creating b = {}", Rc::strong_count(&a));
+    {
+        let c = RcCons(4, Rc::clone(&a));
+        println!("count after creating c = {}", Rc::strong_count(&a));
+    }
+    println!("count after c goes out of scope = {}", Rc::strong_count(&a));
 }
