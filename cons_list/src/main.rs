90e829b548396a8177bb642b3ee3b2645caa5b51
rulewrite <rulewrite@gmail.com>
2022-07-31 14:34:48 +0900

    feat: 가변 데이터의 복수 소유자 만들기

--- cons_list/src/main.rs
+++ cons_list/src/main.rs
@@ -8,8 +8,16 @@ enum RcList {
     Nil,
 }
 
+#[derive(Debug)]
+enum RcRefList {
+    Cons(Rc<RefCell<i32>>, Rc<RcRefList>),
+    Nil,
+}
+
 use crate::List::{Cons, Nil};
 use crate::RcList::{Cons as RcCons, Nil as RcNil};
+use crate::RcRefList::{Cons as RcRefCons, Nil as RcRefNil};
+use std::cell::RefCell;
 use std::rc::Rc;
 
 fn main() {
@@ -24,4 +32,15 @@ fn main() {
         println!("count after creating c = {}", Rc::strong_count(&a));
     }
     println!("count after c goes out of scope = {}", Rc::strong_count(&a));
+
+    let value = Rc::new(RefCell::new(5));
+    let a = Rc::new(RcRefCons(Rc::clone(&value), Rc::new(RcRefNil)));
+    let b = RcRefCons(Rc::new(RefCell::new(3)), Rc::clone(&a));
+    let c = RcRefCons(Rc::new(RefCell::new(4)), Rc::clone(&a));
+
+    *value.borrow_mut() += 10;
+
+    println!("a after = {:?}", a);
+    println!("b after = {:?}", b);
+    println!("c after = {:?}", c);
 }
