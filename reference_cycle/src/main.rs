5f8a2e51260fce4cdfad5c93a7eb44c55dcdffeb
rulewrite <rulewrite@gmail.com>
2022-07-31 15:47:44 +0900

    feat: 트리 구조체 약한 참조 타입의 부모 필드 추가

--- reference_cycle/src/main.rs
+++ reference_cycle/src/main.rs
@@ -1,6 +1,6 @@
 use crate::List::{Cons, Nil};
 use std::cell::RefCell;
-use std::rc::Rc;
+use std::rc::{Rc, Weak};
 
 #[derive(Debug)]
 enum List {
@@ -17,8 +17,10 @@ impl List {
     }
 }
 
+#[derive(Debug)]
 struct Node {
     value: i32,
+    parent: RefCell<Weak<Node>>,
     children: RefCell<Vec<Rc<Node>>>,
 }
 
@@ -44,11 +46,19 @@ fn main() {
 
     let leaf = Rc::new(Node {
         value: 3,
+        parent: RefCell::new(Weak::new()),
         children: RefCell::new(vec![]),
     });
 
+    println!("leaf parent = {:?}", leaf.parent.borrow().upgrade());
+
     let branch = Rc::new(Node {
         value: 5,
+        parent: RefCell::new(Weak::new()),
         children: RefCell::new(vec![Rc::clone(&leaf)]),
     });
+
+    *leaf.parent.borrow_mut() = Rc::downgrade(&branch);
+
+    println!("leaf parent = {:?}", leaf.parent.borrow().upgrade());
 }
