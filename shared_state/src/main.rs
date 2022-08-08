5e9717eb4650afc3bd97116e72ad926ace536ce7
rulewrite <rulewrite@gmail.com>
2022-08-09 04:02:49 +0900

    feat: 여러 스레드에서 Mutex 데이터 사용하기

--- shared_state/src/main.rs
+++ shared_state/src/main.rs
@@ -1,4 +1,6 @@
+use std::sync::Arc;
 use std::sync::Mutex;
+use std::thread;
 
 fn main() {
     let m = Mutex::new(5);
@@ -9,4 +11,24 @@ fn main() {
     }
 
     println!("m = {:?}", m);
+
+    // 복수 스레드에서 뮤텍스 공유
+    let counter = Arc::new(Mutex::new(0));
+    let mut handles = vec![];
+
+    for _ in 0..10 {
+        let counter = Arc::clone(&counter);
+        let handle = thread::spawn(move || {
+            let mut num = counter.lock().unwrap();
+
+            *num += 1;
+        });
+        handles.push(handle);
+    }
+
+    for handle in handles {
+        handle.join().unwrap();
+    }
+
+    println!("Result: {}", *counter.lock().unwrap());
 }
