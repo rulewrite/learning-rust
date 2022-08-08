760f1b8786c3815fb96f651f90e3790d6bb7b7d7
rulewrite <rulewrite@gmail.com>
2022-08-09 01:59:49 +0900

    chore: 전달된 값 소유권 이전 확인 주석 코드

--- message_passing/src/main.rs
+++ message_passing/src/main.rs
@@ -7,6 +7,7 @@ fn main() {
     thread::spawn(move || {
         let val = String::from("hi");
         tx.send(val).unwrap();
+        // println!("val is {}", val);
     });
 
     let received = rx.recv().unwrap();
