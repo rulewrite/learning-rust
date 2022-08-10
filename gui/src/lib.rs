b72f4335d82b8b79d62e92c37c93856b3a13cf63
rulewrite <rulewrite@gmail.com>
2022-08-10 21:29:26 +0900

    feat: Screen 구조체 run 메소드 추가

--- gui/src/lib.rs
+++ gui/src/lib.rs
@@ -5,3 +5,11 @@ pub trait Draw {
 pub struct Screen {
     pub components: Vec<Box<dyn Draw>>,
 }
+
+impl Screen {
+    pub fn run(&self) {
+        for component in self.components.iter() {
+            component.draw();
+        }
+    }
+}
