24e8a28fe07a7ddd443299f94d6e4b5ca32b2f0a
rulewrite <rulewrite@gmail.com>
2022-08-10 22:47:45 +0900

    feat: Button 구조체 추가

--- gui/src/lib.rs
+++ gui/src/lib.rs
@@ -13,3 +13,15 @@ impl Screen {
         }
     }
 }
+
+pub struct Button {
+    pub width: u32,
+    pub height: u32,
+    pub label: String,
+}
+
+impl Draw for Button {
+    fn draw(&self) {
+        // code to actually draw a button
+    }
+}
