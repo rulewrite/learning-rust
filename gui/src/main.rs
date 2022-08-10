ac05b4838dd8507dc8e39e37276c37a9d8c7a275
rulewrite <rulewrite@gmail.com>
2022-08-10 23:18:34 +0900

    feat: Screen 인스턴스 추가 및 run 실행

--- gui/src/main.rs
+++ gui/src/main.rs
@@ -12,4 +12,27 @@ impl Draw for SelectBox {
     }
 }
 
-fn main() {}
+use gui::{Button, Screen};
+
+fn main() {
+    let screen = Screen {
+        components: vec![
+            Box::new(SelectBox {
+                width: 75,
+                height: 10,
+                options: vec![
+                    String::from("Yes"),
+                    String::from("Maybe"),
+                    String::from("No"),
+                ],
+            }),
+            Box::new(Button {
+                width: 50,
+                height: 10,
+                label: String::from("OK"),
+            }),
+        ],
+    };
+
+    screen.run();
+}
