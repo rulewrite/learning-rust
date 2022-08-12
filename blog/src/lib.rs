462120bae0f74624ff0e42a67a7f885954d729c5
rulewrite <rulewrite@gmail.com>
2022-08-12 17:46:59 +0900

    feat: 임시 content 메소드 구현

--- blog/src/lib.rs
+++ blog/src/lib.rs
@@ -14,6 +14,10 @@ impl Post {
     pub fn add_text(&mut self, text: &str) {
         self.content.push_str(text);
     }
+
+    pub fn content(&self) -> &str {
+        ""
+    }
 }
 
 trait State {}
