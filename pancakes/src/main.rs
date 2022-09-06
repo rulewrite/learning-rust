58353bc175b0fc1096b4b3f5675b373ac0fd65cb
rulewrite <rulewrite@gmail.com>
2022-09-06 23:43:51 +0900

    feat: 절차적 매크로를 사용하여 트레잇 직접 구현하지 않고 사용

--- pancakes/src/main.rs
+++ pancakes/src/main.rs
@@ -1,12 +1,8 @@
 use hello_macro::HelloMacro;
+use hello_macro_derive::HelloMacro;
 
+#[derive(HelloMacro)]
 struct Pancakes;
-
-impl HelloMacro for Pancakes {
-    fn hello_macro() {
-        println!("Hello, Macro! My name is Pancakes!");
-    }
-}
 
 fn main() {
     Pancakes::hello_macro();
