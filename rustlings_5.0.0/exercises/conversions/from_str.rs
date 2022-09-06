432cf8c1ad96797bad6882151659aa7e76e2d86d
rulewrite <rulewrite@gmail.com>
2022-09-07 01:11:08 +0900

    feat: conversions 1, 2문제 해결

--- rustlings_5.0.0/exercises/conversions/from_str.rs
+++ rustlings_5.0.0/exercises/conversions/from_str.rs
@@ -45,8 +45,7 @@ enum ParsePersonError {
 
 impl FromStr for Person {
     type Err = ParsePersonError;
-    fn from_str(s: &str) -> Result<Person, Self::Err> {
-    }
+    fn from_str(s: &str) -> Result<Person, Self::Err> {}
 }
 
 fn main() {
