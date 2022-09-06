1d721c8a4137b86ae8ab58c7a4b173136313bd17
rulewrite <rulewrite@gmail.com>
2022-09-06 23:39:44 +0900

    feat: hello_macro 모듈의 크레이트 구현

--- pancakes/src/main.rs
+++ pancakes/src/main.rs
@@ -1,3 +1,13 @@
+use hello_macro::HelloMacro;
+
+struct Pancakes;
+
+impl HelloMacro for Pancakes {
+    fn hello_macro() {
+        println!("Hello, Macro! My name is Pancakes!");
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    Pancakes::hello_macro();
 }
