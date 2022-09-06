86bb6ff061272694de643111d390f59d99593cf8
rulewrite <rulewrite@gmail.com>
2022-09-06 23:26:08 +0900

    feat: 절차적 매크로 추가

--- hello_macro/hello_macro_derive/src/lib.rs
+++ hello_macro/hello_macro_derive/src/lib.rs
@@ -1,8 +1,24 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
-    }
+use proc_macro::TokenStream;
+use quote::quote;
+use syn;
+
+#[proc_macro_derive(HelloMacro)]
+pub fn hello_macro_derive(input: TokenStream) -> TokenStream {
+    // 러스트 코드를 파싱해 조작할 수 있는 트리로 구성
+    let ast = syn::parse(input).unwrap();
+
+    // 트레이트 구현 빌드
+    impl_hello_macro(&ast)
+}
+
+fn impl_hello_macro(ast: &syn::DeriveInput) -> TokenStream {
+    let name = &ast.ident;
+    let gen = quote! {
+        impl HelloMacro for #name {
+            fn hello_macro() {
+                println!("Hello, Macro! My name is {}!", stringify!(#name));
+            }
+        }
+    };
+    gen.into()
 }
